a694a134bf04691d3ebe4503e3188bf82932463e
Grcllch02 <gchelsea01@student.ciputra.ac.id>
2025-09-14 16:03:02 +0700

    delete view mentor

--- controller.php
+++ controller.php
@@ -21,16 +21,54 @@ function createMentor(){
     $sql = "INSERT INTO mentor(nama, jurusan, no_telepon) VALUES ('$mentor->nama', '$mentor->jurusan', '$mentor->no_tlpn')";
 
     if (mysqli_query($conn, $sql)) {
-        echo "Data mentor berhasil disimpan!";
+        mysqli_close($conn);
+        // echo "Data mentor berhasil disimpan!";
     } else {
         echo "Error: " . mysqli_error($conn);
     }
-
-    mysqli_close($conn);
+    
 }
 
 if (isset($_POST['button_registerMentor'])) {
     createMentor();
     header("Location:view_mentor.php");  
 }
+
+function viewMentor(){
+    $conn = my_connectDB();
+
+    $sql = "SELECT * FROM mentor";
+    $result = mysqli_query($conn, $sql);
+
+    $mentorlist = [];
+    if($result && mysqli_num_rows($result) > 0){
+        while ($row = mysqli_fetch_assoc($result)) {
+            $mentorList[] = $row;
+        }
+    }
+
+    mysqli_close($conn);
+    return $mentorList;
+}
+
+function deleteMentor($id){
+    $conn = my_connectDB();
+
+    $sql = "DELETE from mentor WHERE mentor_id = $id";
+
+    if(mysqli_query($conn, $sql)){
+        
+    }else{
+        echo "Error: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+
+if(isset($_GET['deleteID'])){
+    $id = $_GET['deleteID'];
+    deleteMentor($id);
+    header("Location:view_mentor.php");
+}
+
 ?>
--- view_mentor.php
+++ view_mentor.php
@@ -1,3 +1,7 @@
+<?php require("controller.php"); 
+$mentors = viewMentor();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,28 +32,28 @@
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">No</th>
+                            <th scope="col">ID</th>
                             <th scope="col">Nama</th>
                             <th scope="col">Jurusan</th>
                             <th scope="col">Nomor Telepon</th>
-                            <th>Note</th>
                             <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                            <td>
-                                <a href="" class="btn btn-warning">Update</a>
-                                <a href="" class="btn btn-danger">Delete</a>
-                                <a href="" class="btn btn-primary">Edit</a>
-                                <a href="view_addmentor.php" class="btn btn-success">Add</a>
-                            </td>
-                        </tr>
+                        <?php foreach ($mentors as $mentor1): ?>
+                            <tr>
+                                <td><?= $mentor1['mentor_id'] ?></td>
+                                <td><?= $mentor1['nama'] ?></td>
+                                <td><?= $mentor1['jurusan'] ?></td>
+                                <td><?= $mentor1['no_telepon'] ?></td>
+                                <td>
+                                    <a href="" class="btn btn-warning">Edit</a>
+                                    <a href="controller.php?deleteID=<?=$mentor1['mentor_id']?>"><button class="btn btn-danger" onclick="return confirm('Yakin mau hapus mentor ini?');">Delete</button></a>
+                                    
+                                    <a href="view_addmentor.php" class="btn btn-success">Add</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
