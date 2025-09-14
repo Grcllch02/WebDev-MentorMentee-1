9f781f2c0e4e2cfd73eeb210842e7a64f54122b7
Grcllch02 <gchelsea01@student.ciputra.ac.id>
2025-09-14 12:54:35 +0700

    Mentor
    model + view mentor

---
+++ model.php
@@ -0,0 +1,16 @@
+<?php
+
+class mentor{
+    public $mentor_id;
+    public $nama;
+    public $jurusan;
+    public $no_tlpn;
+}
+
+class mentee{
+    public $mentee_id;
+    public $nama;
+    public $jurusan;
+    public $no_tlpn;
+}
+?>
---
+++ view_mentor.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Mentor List</title>
+</head>
+
+<body>
+    <div class="container p-3">
+        <div class="card text-center">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="true" href="view_mentor.php">Mentor List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Add Member</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Jurusan</th>
+                            <th scope="col">Nomor Telepon</th>
+                            <th scope="col">Action</th>
+                            <th>Note</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-warning">Update</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+</body>
+
+</html>
